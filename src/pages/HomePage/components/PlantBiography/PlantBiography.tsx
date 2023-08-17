a50eaaba6a52fc6ff1e89f0351d97ed49452f5a1
DariaKostrik <dolorez0927@yandex.ru>
2023-08-17 16:17:35 +0300

    finish plant biography

--- src/pages/HomePage/components/PlantBiography/PlantBiography.tsx
+++ src/pages/HomePage/components/PlantBiography/PlantBiography.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import "./PlantBiography.scss"
+import fl1 from "./assets/fl1 (1).png"
+import fl2 from "./assets/fl2.png"
+import fl3 from "./assets/fl1 (3).png"
 
 export const PlantBiography = () => {
   return (
-    <div>PlantBiography</div>
+    <section className="plant-biography">
+        <div className="base">
+            <p id="p1">Plant Biography</p>
+            <p id="p2">Smoothlu And healthy plant fore you and all people</p>
+            <div id="content-block">
+                <p className="form-block left">
+                    <img src={fl1} alt='fl1'/>
+                </p>
+                <p className="form-block center">
+                <img src={fl2} alt='fl2'/>
+                    <button className="button-bio">Biography</button>
+                </p>
+                <p className="form-block right">
+                <img src={fl3} alt='fl3'/>
+                </p>
+            </div>
+        </div>
+    </section>
   )
 }
